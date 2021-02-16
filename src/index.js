6dfc2afea0c7d5c04b379bf23ef410bf1f353418
Aleksandr Zalutskiy <simplesash@gmail.com>
2021-02-16 18:13:45 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arr = ('' + n).split('');
+    let newArr = '';
+
+    if (arr[0] === '-') {
+        newArr = arr.slice(1);
+        return newArr.reverse().join('');
+    } else {
+        return arr.reverse().join('');
+    };
 }
